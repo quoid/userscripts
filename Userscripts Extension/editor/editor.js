cd29c7975250ffef8f2075747231017a550a5aea
quoid <justin@k21p.com>
2019-05-08 16:38:23 -0400

    wrapped the editor js and prefixed all globally scoped vars with “___” #3

--- Userscripts Extension/editor/editor.js
+++ Userscripts Extension/editor/editor.js
@@ -1,109 +1,100 @@
-var loaded = false;
-var savedCode = "";
-var infoButton = document.getElementById("info");
-var saveButton = document.getElementById("save");
-var discardButton = document.getElementById("discard");
-var downloadButton = document.getElementById("download");
-var sessionCode = "";
+var ___userscripts = {
+    loaded: false,
+    savedCode: "",
+    sessionCode: "",
+    infoButton: document.getElementById("info"),
+    saveButton: document.getElementById("save"),
+    discardButton: document.getElementById("discard"),
+    downloadButton: document.getElementById("download"),
+    setEditorMessage: function(m) {
+        document.getElementById("message").innerHTML = m;
+    },
+    setVersion: function(v) {
+        document.getElementById("version").innerHTML = v;
+    },
+    enableButtons: function() {
+        this.saveButton.removeAttribute("disabled");
+        this.discardButton.removeAttribute("disabled");
+    },
+    disableButtons: function() {
+        this.saveButton.setAttribute("disabled", true);
+        this.discardButton.setAttribute("disabled", true);
+    },
+    getInfo: function() {
+        window.webkit.messageHandlers.getInfo.postMessage("user wants info");
+    },
+    downloadScript: function() {
+        window.webkit.messageHandlers.downloadScript.postMessage("user wants to download script");
+    },
+    discard: function() {
+        this.editor.setValue(this.savedCode);
+        this.disableButtons();
+    },
+    save: function() {
+        var code = this.editor.getValue();
+        window.webkit.messageHandlers.saveCode.postMessage(escape(code));
+        this.savedCode = code;
+        this.sessionCode = "";
+        this.setEditorMessage("All changes saved");
+        this.disableButtons();
+    },
+    setSaveDate: function(dateString) {
+        setTimeout(function() {
+            this.setEditorMessage("Last edited on " + dateString);
+        }, 1500);
+    },
+    loadCode: function(c) {
+        var code = unescape(c);
+        if (code.trim().length > 0 && this.loaded === false) {
+            this.savedCode = code;
+            this.editor.setValue(code);
+            this.disableButtons();
+            this.loaded = true;
+        }
+    },
+    webViewOnLoad: function() {
+        if (this.loaded === false) {
+            window.webkit.messageHandlers.webViewOnLoad.postMessage(this.loaded);
+        }
+    },
+    editor: CodeMirror.fromTextArea(document.getElementById("code"), {
+        mode:  "javascript",
+        smartIndent: true,
+        tabSize: 2,
+        lineWrapping: true,
+        lineNumbers: true,
+        autoCloseBrackets: true,
+        styleActiveLine: true,
+        matchBrackets: true,
+        hintOptions: {useGlobalScope: true},
+        extraKeys: {"Ctrl-Space": "autocomplete", "Cmd-S": function() {___userscripts.save();}, "Cmd-/": "toggleComment"}
+    })
+};
 
-function setEditorMessage(m) {
-    document.getElementById("message").innerHTML = m;
-}
-
-function setVersion(v) {
-    document.getElementById("version").innerHTML = v;
-}
-
-function enableButtons() {
-    saveButton.removeAttribute("disabled");
-    discardButton.removeAttribute("disabled");
-}
-
-function disableButtons() {
-    saveButton.setAttribute("disabled", true);
-    discardButton.setAttribute("disabled", true);
-}
-
-var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-    mode:  "javascript",
-    smartIndent: true,
-    tabSize: 2,
-    lineWrapping: true,
-    lineNumbers: true,
-    autoCloseBrackets: true,
-    styleActiveLine: true,
-    matchBrackets: true,
-    hintOptions: {useGlobalScope: true},
-    extraKeys: {"Ctrl-Space": "autocomplete", "Cmd-S": save, "Cmd-/": "toggleComment"}
-});
-
-editor.on("change", function(e, c) {
+___userscripts.editor.on("change", function(e, c) {
     var inputAction = c.origin;
     
     if (inputAction !== "setValue") {
-        sessionCode = e.getValue();
-        enableButtons();
+        ___userscripts.sessionCode = e.getValue();
+        ___userscripts.enableButtons();
     }
     
     if (inputAction === "undo" || inputAction === "redo") {
-        if (sessionCode.localeCompare(savedCode) === 0) {
-            disableButtons();
+        if (___userscripts.sessionCode.localeCompare(___userscripts.savedCode) === 0) {
+            ___userscripts.disableButtons();
         }
     }
 });
 
-editor.on("keydown", function(cm, e) {
+___userscripts.editor.on("keydown", function(cm, e) {
     var currentLinePosition = cm.getCursor()["ch"];
     if (!cm.state.completionActive && e.metaKey === false && e.keyCode >= 65 && e.keyCode <= 90 && currentLinePosition != 0) {
         cm.showHint({completeSingle: false});
     }
 });
 
-function getInfo() {
-    window.webkit.messageHandlers.getInfo.postMessage("user wants info");
-}
-function downloadScript() {
-    window.webkit.messageHandlers.downloadScript.postMessage("user wants to download script");
-}
-
-function discard() {
-    editor.setValue(savedCode);
-    disableButtons();
-}
-
-function save() {
-    var code = editor.getValue();
-    window.webkit.messageHandlers.saveCode.postMessage(escape(code));
-    savedCode = code;
-    sessionCode = "";
-    setEditorMessage("All changes saved");
-    disableButtons();
-}
-
-function setSaveDate(dateString) {
-    setTimeout(function() {
-        setEditorMessage("Last edited on " + dateString);
-    }, 1500);
-}
-
-function loadCode(c) {
-    var code = unescape(c);
-    if (code.trim().length > 0 && loaded === false) {
-        savedCode = code;
-        editor.setValue(code);
-        disableButtons();
-        loaded = true;
-    }
-}
-
-function webViewOnLoad() {
-    if (loaded === false) {
-        window.webkit.messageHandlers.webViewOnLoad.postMessage(loaded);
-    }
-}
-
-infoButton.addEventListener("click", getInfo);
-saveButton.addEventListener("click", save);
-discardButton.addEventListener("click", discard);
-downloadButton.addEventListener("click", downloadScript)
-window.onload = webViewOnLoad;
+___userscripts.infoButton.addEventListener("click", function(){___userscripts.getInfo();});
+___userscripts.saveButton.addEventListener("click", function(){___userscripts.save();});
+___userscripts.discardButton.addEventListener("click", function(){___userscripts.discard();});
+___userscripts.downloadButton.addEventListener("click", function(){___userscripts.downloadScript();});
+window.onload = ___userscripts.webViewOnLoad();
