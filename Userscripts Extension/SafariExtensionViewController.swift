ac791c8a186a21dbfa9350f274886a6e170c6400
quoid <justin@k21p.com>
2019-08-03 16:21:50 -0400

    resolves #24, resolves #25, partially #27, and sort of #21

--- Userscripts Extension/SafariExtensionViewController.swift
+++ Userscripts Extension/SafariExtensionViewController.swift
@@ -26,12 +26,13 @@ class SafariExtensionViewController: SFSafariExtensionViewController, WKScriptMe
         let bundleURL = Bundle.main.resourceURL!.absoluteURL
         let html = bundleURL.appendingPathComponent("editor.html")
         webViewConfig.preferences.setValue(true, forKey: "developerExtrasEnabled")
-        webViewConfig.userContentController.add(self, name: "webViewOnLoad")
+        webViewConfig.userContentController.add(self, name: "webViewReady")
+        webViewConfig.userContentController.add(self, name: "setCursor")
         webViewConfig.userContentController.add(self, name: "saveCode")
+        webViewConfig.userContentController.add(self, name: "setStatus")
+        webViewConfig.userContentController.add(self, name: "setModificationDate")
         webViewConfig.userContentController.add(self, name: "getInfo")
         webViewConfig.userContentController.add(self, name: "downloadScript")
-        webViewConfig.userContentController.add(self, name: "setCursor")
-        webViewConfig.userContentController.add(self, name: "setStatus")
         webView = WKWebView(frame: CGRect(x: 0, y: 0, width: parentWidth, height: parentHeight), configuration: webViewConfig)
         webView.navigationDelegate = self
         webView.allowsLinkPreview = false
@@ -40,67 +41,137 @@ class SafariExtensionViewController: SFSafariExtensionViewController, WKScriptMe
         self.view.addSubview(webView)
     }
     
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        NSAnimationContext.runAnimationGroup({_ in
-            NSAnimationContext.current.duration = 0.35
-            webView.animator().alphaValue = 1.0
-        })
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         let backgroundColor = NSColor.init(red: (39/255.0), green: (42/255.0), blue: (46/255.0), alpha: 1.0)
         view.setValue(backgroundColor, forKey: "backgroundColor")
+        if UserDefaults.standard.object(forKey: "toggleOff") == nil {
+            UserDefaults.standard.set(false, forKey: "toggleOff")
+        }
         initWebView()
     }
     
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        if message.name == "saveCode" {
-            saveData(code: message.body as! String)
-            webView.evaluateJavaScript("___userscripts.setSaveDate('\(EditorData.lastEdited)');", completionHandler: nil)
-        }
-        
-        if message.name == "webViewOnLoad" {
-            webView.evaluateJavaScript("___userscripts.loadCode('\(EditorData.code)');", completionHandler: nil)
-            if !EditorData.lastEdited.isEmpty {
-                webView.evaluateJavaScript("___userscripts.setEditorMessage('Last edited on \(EditorData.lastEdited)');", completionHandler: nil)
-            }
-            let status = UserDefaults.standard.string(forKey: "ToggleStatus")
-            if status == "off" {
-                webView.evaluateJavaScript("___userscripts.toggleOff();", completionHandler: nil)
-            }
-            let appVersion = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
-            webView.evaluateJavaScript("___userscripts.setVersion(' v\(appVersion)');", completionHandler: nil)
-        }
-        
-        if message.name == "getInfo" {
-            openExtensionHomepage()
-            dismissPopover()
-        }
-        
-        if message.name == "downloadScript" {
-            downloadScript()
-            dismissPopover()
-        }
-        
-        if message.name == "setStatus" {
-            let st = message.body as! String
-            setToggleStatus(status: st)
-        }
-        
-        //hack to get popover webview to set proper cursors
-        if message.name == "setCursor" {
-            let cursor = message.body as! String
-            if (cursor == "auto" || cursor == "default") {
-                NSCursor.arrow.set()
-            }
-            if (cursor == "pointer") {
-                NSCursor.pointingHand.set()
-            }
-            if (cursor == "text") {
-                NSCursor.iBeam.set()
-            }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        let lang = Locale.current.languageCode!
+        let ver = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
+        let toggle = UserDefaults.standard.bool(forKey: "toggleOff")
+        if var code = getSavedCode() {
+            code = code.addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
+            webView.evaluateJavaScript("___userscripts.init('\(lang)', '\(ver)', '\(toggle)', '\(code)');", completionHandler: nil)
+        } else {
+            webView.evaluateJavaScript("___userscripts.init('\(lang)', '\(ver)', '\(toggle)');", completionHandler: nil)
         }
     }
     
+    func saveCode(code:String) {
+        let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        let path = documentDirectory.appending("/userscript.js")
+        let fileURL = URL(fileURLWithPath: path)
+        let fileContent = code
+        do {
+            try fileContent.write(to: fileURL, atomically: false, encoding: .utf8)
+            let escapedCode = code.addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
+            webView.evaluateJavaScript("___userscripts.saveSucceed('\(escapedCode)');", completionHandler: { (value, error) in
+                guard error == nil else {return}
+                let d = getModificationDate()
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.25) {
+                    self.webView.evaluateJavaScript("___userscripts.setStatusText('\(d)', '\(true)');", completionHandler: nil)
+                }
+            })
+        } catch {
+            webView.evaluateJavaScript("___userscripts.saveFail();", completionHandler: nil)
+        }
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == "webViewReady" {
+            if getSavedCode() != nil && UserDefaults.standard.bool(forKey: "toggleOff") == false {
+                let d = getModificationDate()
+                self.webView.evaluateJavaScript("___userscripts.setStatusText('\(d)', '\(true)');", completionHandler: nil)
+            }
+            NSAnimationContext.runAnimationGroup({_ in
+                NSAnimationContext.current.duration = 0.35
+                webView.animator().alphaValue = 1.0
+            })
+        }
+        if message.name == "setCursor" {
+            let cursor = message.body as! String
+            switch cursor {
+            case "auto", "default":
+                NSCursor.arrow.set()
+            case "pointer":
+                NSCursor.pointingHand.set()
+            case "text":
+                NSCursor.iBeam.set()
+            default:
+                break
+            }
+        }
+        if message.name == "saveCode" {
+            let code = message.body as! String
+            saveCode(code: code)
+        }
+        if message.name == "setStatus" {
+            let toggleStatus = message.body as! Bool
+            UserDefaults.standard.set(toggleStatus, forKey: "toggleOff")
+        }
+        if message.name == "setModificationDate" {
+            let d = getModificationDate()
+            webView.evaluateJavaScript("___userscripts.setStatusText('\(d)', '\(true)');", completionHandler: nil)
+        }
+        if message.name == "getInfo" {
+            openExtensionHomepage()
+        }
+        if message.name == "downloadScript" {
+            downloadScript()
+        }
+    }
+}
+
+func getModificationDate() -> String {
+    let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+    let path = documentDirectory.appending("/userscript.js")
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = .medium
+    dateFormatter.timeStyle = .short
+    do {
+        let date = try FileManager.default.attributesOfItem(atPath: path)[.modificationDate] as! Date
+        return dateFormatter.string(from: date)
+    } catch {
+        return "error getting date"
+    }
+}
+
+func getSavedCode() -> String? {
+    let fileManager = FileManager.default
+    let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+    let path = documentDirectory.appending("/userscript.js")
+    let fileURL = URL(fileURLWithPath: path)
+    var savedCode:String?
+    if fileManager.fileExists(atPath: path) {
+        do {
+            savedCode = try String(contentsOf: fileURL, encoding: .utf8)
+        } catch {
+            print(error)
+        }
+    }
+    return savedCode
+}
+
+func openExtensionHomepage() {
+    let url = URL(string: "https://github.com/quoid/userscripts")!
+    SFSafariApplication.getActiveWindow { (window) in
+        window?.openTab(with: url, makeActiveIfPossible: true, completionHandler: nil)
+    }
+}
+
+func downloadScript() {
+    guard let code = getSavedCode() else {return}
+    SFSafariApplication.getActiveWindow { (window) in
+        window?.getActiveTab { (tab) in
+            tab?.getActivePage { (page) in
+                page?.dispatchMessageToScript(withName: "DOWNLOAD_SCRIPT", userInfo: ["code": code])
+            }
+        }
+    }
 }
---
+++ Userscripts Extension/editor/localize.js
@@ -0,0 +1,35 @@
+const ___strings = {
+    "en": {
+        "button_save": "Save",
+        "button_discard": "Discard",
+        "editor_placeholder": "//Code input here gets injected into every website you visit...",
+        "modification_date": "Last modified on",
+        "status_disabled": "Code injection disabled, click the power button to enable...",
+        "status_ready": "Ready for code...",
+        "status_saving": "Saving changes...",
+        "status_save_succeed": "All changes saved...",
+        "status_save_fail": "Failed to save changes..."
+    },
+    "es": {
+        "button_save": "Guardar",
+        "button_discard": "Cancelar",
+        "editor_placeholder": "//El código que ingrese aquí se insertará en cada sitio web que visite...",
+        "modification_date": "Última edición en",
+        "status_disabled": "Inyección de código desactivada, haga clic en el botón de encendido para habilitar...",
+        "status_ready": "Listo para el código...",
+        "status_saving": "Guardando cambios...",
+        "status_save_succeed": "Todos los cambios guardados...",
+        "status_save_fail": "Error al guardar los cambios..."
+    },
+    "sv": {
+        "button_save": "Spara",
+        "button_discard": "Avbryt",
+        "editor_placeholder": "//Koden som du anger här kommer att injiceras på varje webbplats du besöker...",
+        "modification_date": "Senast redigerad den",
+        "status_disabled": "Kodinjektion inaktiverad, klicka på strömknappen för att aktivera...",
+        "status_ready": "Klar för kod ...",
+        "status_saving": "Sparar ändringar...",
+        "status_save_succeed": "Alla ändringar sparades...",
+        "status_save_fail": "Det gick inte att spara ändringarna..."
+    }
+}
