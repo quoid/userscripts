a0c18b451d26497f4005cf8e81c54b5c3539f996
quoid <justin@k21p.com>
2020-06-29 21:30:47 -0400

    rework script injection, related to #39

--- extension/Userscripts Extension/script.js
+++ extension/Userscripts Extension/script.js
@@ -1,46 +1,78 @@
+// all code from all scripts saved to data var
+let data;
+// var that determines whether strict csp injection has already run (JS only)
+let evalJS = 0;
+
+// attempt to ensure script only runs on top-level pages
 if (window.top === window) {
+    // request saved script code
     safari.extension.dispatchMessage("REQ_USERSCRIPTS");
-}
-
-function injectCSS(code) {
-    const doc = document.createElement("style");
-    doc.textContent = code;
-    document.head.appendChild(doc);
-}
-
-function injectJS(name, code) {
-    code = code + "\n//# sourceURL=" + name.replace(/\s/g, "-");
-    if (document.readyState === "loading") {
-        document.addEventListener("DOMContentLoaded", function() {
-            eval(code);
-        });
-    } else {
-        eval(code);
-    }
-}
-
-function handleMessage(event) {
-    if (event.name === "RESP_USERSCRIPTS") {
-        const obj = event.message.data;
-        const error = event.message.error;
-        if (error) {
-            console.error(error);
-            return;
+    // event listener to detect strict CSPs
+    document.addEventListener("securitypolicyviolation", function(e) {
+        const src = e.sourceFile.toUpperCase();
+        const ext = safari.extension.baseURI.toUpperCase();
+        // ensure that violation came from the extension
+        if (ext.startsWith(src)) {
+            // determine what kind of violation
+            if (e.effectiveDirective === "script-src" && evalJS != 1) {
+                inject("eval");
+                // change var to ensure eval injection only happens once
+                evalJS = 1;
+            }
         }
-        for (const type in obj) {
-            var typeObj = obj[type];
-            for (const filename in typeObj) {
-                var code = typeObj[filename];
-                if (type === "css") {
-                    injectCSS(code);
-                }
-                if (type === "js") {
-                    injectJS(filename, code);
-                }
+    });
+}
+
+function inject(method) {
+    // iterate over data and extract css/js individually for injection
+    for (const type in data) {
+        const codeType = data[type];
+        for (const filename in codeType) {
+            let code = codeType[filename];
+            // css injection only happens with inline method
+            // if blocked by csp, no reliable way to inject style code yet
+            // future fix?: https://wicg.github.io/construct-stylesheets/
+            if (type === "css" && method == "inline") {
+                const tag = document.createElement("style");
+                tag.textContent = code;
+                document.head.appendChild(tag);
                 console.log(`Injecting ${filename}`);
+            } else if (type === "js") {
+                code = code + "\n//# sourceURL=" + filename.replace(/\s/g, "-");
+                if (method === "inline") {
+                    const tag = document.createElement("script");
+                    tag.textContent = code;
+                    document.body.appendChild(tag);
+                    console.log(`Injecting ${filename}`);
+                } else if (method === "eval"){
+                    eval(code);
+                }
             }
         }
     }
 }
 
+// respond to messages
+function handleMessage(event) {
+    // the only message currently sent to the content script
+    if (event.name === "RESP_USERSCRIPTS") {
+        // if error returned, log and stop execution
+        if (event.message.error) {
+            console.error(event.message.error);
+            return;
+        }
+        // save data sent with message to var
+        data = event.message.data;
+        // attempt to inject all code inline after page has loaded
+        if (document.readyState !== "loading") {
+            inject("inline");
+        } else {
+            document.addEventListener("DOMContentLoaded", function() {
+                inject("inline");
+            });
+        }
+    }
+}
+
+// event listener to handle messages
 safari.self.addEventListener("message", handleMessage);
