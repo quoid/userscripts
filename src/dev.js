e508499ffb0c4aaf5082a81b802d6c9aaf973c1a
quoid <justin@k21p.com>
2021-03-06 15:38:44 -0500

    disable showCount by default

--- src/dev.js
+++ src/dev.js
@@ -13,7 +13,7 @@ const defaultSettings = {
     log: "false",
     saveLocation: "/Users/UserName/Library/Containers/ParentFolder/SubFolder/",
     showInvisibles: "true",
-    showCount: "true",
+    showCount: "false",
     sortOrder: "lastModifiedDesc",
     tabSize: "4",
     version: "3.1.0"
