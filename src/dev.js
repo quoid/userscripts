48c75915e2f640a0f1a6cb508246f01c444fff2f
quoid <justin@k21p.com>
2021-06-14 22:01:00 -0400

    bump version and build numbers

--- src/dev.js
+++ src/dev.js
@@ -18,7 +18,7 @@ const defaultSettings = {
     showCount: "false",
     sortOrder: "lastModifiedDesc",
     tabSize: "4",
-    version: "3.2.1"
+    version: "3.2.2"
 };
 
 // example file contents
