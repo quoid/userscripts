78175c83fba78f34d60f4368b9a61f0d03cf04ca
quoid <justin@emlxl.com>
2021-08-04 21:49:08 -0400

    improved error handling

--- extension/Userscripts Extension/SafariWebExtensionHandler.swift
+++ extension/Userscripts Extension/SafariWebExtensionHandler.swift
@@ -12,61 +12,62 @@ class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
         logText("Got message with name: \(name)")
         // got a valid message, construct response based on message received
         let response = NSExtensionItem()
+        // send standard error when there's an issue parsing inbound message
+        var inBoundError = false
         // these if/else if statement are formatted so that they can be neatly collapsed in Xcode
         // typically the "else if" would be on the same line as the preceding statements close backet
         // ie. } else if {
         if name == "REQ_USERSCRIPTS" {
-            guard
-                let url = message?["url"] as? String,
-                let isTop = message?["isTop"] as? Bool,
-                checkDefaultDirectories(),
-                let matches = getInjectionFilenames(url),
-                let code = getCode(matches, isTop)
-            else {
-                let e = "couldn't get userscripts"
-                response.userInfo = [SFExtensionMessageKey: ["error": e]]
-                context.completeRequest(returningItems: [response], completionHandler: nil)
-                return
+            if let url = message?["url"] as? String, let isTop = message?["isTop"] as? Bool {
+                if
+                    checkDefaultDirectories(),
+                    let matches = getInjectionFilenames(url),
+                    let code = getCode(matches, isTop)
+                {
+                    response.userInfo = [SFExtensionMessageKey: ["code": code]]
+                } else {
+                    response.userInfo = [SFExtensionMessageKey: ["error": "couldn't get userscripts"]]
+                }
+            } else {
+                inBoundError = true
             }
-            response.userInfo = [SFExtensionMessageKey: ["code": code]]
         }
         else if name == "POPUP_BADGE_COUNT" {
-            guard
-                let url = message?["url"] as? String,
-                let frameUrls = message?["frameUrls"] as? [String],
-                let matches = getPopupBadgeCount(url, frameUrls)
-            else {
-                // TODO: handle error
-                return
+            if let url = message?["url"] as? String, let frameUrls = message?["frameUrls"] as? [String] {
+                if let matches = getPopupBadgeCount(url, frameUrls) {
+                    response.userInfo = [SFExtensionMessageKey: ["count": matches]]
+                } else {
+                    response.userInfo = [SFExtensionMessageKey: ["error": "failed to update badge count"]]
+                }
+            } else {
+                inBoundError = true
             }
-            response.userInfo = [SFExtensionMessageKey: ["count": matches]]
         }
         else if name == "POPUP_MATCHES" {
             let manifest = getManifest()
-            guard
-                let url = message?["url"] as? String,
-                let frameUrls = message?["frameUrls"] as? [String],
-                checkDefaultDirectories(),
-                checkSettings(),
-                let matches = getPopupMatches(url, frameUrls, true),
-                let active = manifest.settings["active"],
-                let updates = checkForRemoteUpdates()
-            else {
-                // TODO: handle error
-                return
+            if let url = message?["url"] as? String, let frameUrls = message?["frameUrls"] as? [String] {
+                if
+                    checkDefaultDirectories(),
+                    checkSettings(),
+                    let matches = getPopupMatches(url, frameUrls, true),
+                    let active = manifest.settings["active"],
+                    let updates = checkForRemoteUpdates()
+                {
+                    let r = ["active": active, "items": matches, "updates": updates] as [String : Any]
+                    response.userInfo = [SFExtensionMessageKey: r]
+                } else {
+                    response.userInfo = [SFExtensionMessageKey: ["error": "failed to run match sequence"]]
+                }
+            } else {
+                inBoundError = true
             }
-            let r = ["active": active, "items": matches, "updates": updates] as [String : Any]
-            response.userInfo = [SFExtensionMessageKey: r]
         }
         else if name == "POPUP_UPDATE_ALL" {
-            guard
-                popupUpdateAll(),
-                let updates = checkForRemoteUpdates()
-            else {
-                // TODO: handle error
-                return
+            if popupUpdateAll(), let updates = checkForRemoteUpdates() {
+                response.userInfo = [SFExtensionMessageKey: ["updates": updates]]
+            } else {
+               response.userInfo = [SFExtensionMessageKey: ["error": "failed to run update sequence"]]
             }
-            response.userInfo = [SFExtensionMessageKey: ["updates": updates]]
         }
         else if name == "POPUP_TOGGLE_EXTENSION" {
             var manifest = getManifest()
@@ -79,24 +80,24 @@ class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
             if updateManifest(with: manifest) {
                 response.userInfo = [SFExtensionMessageKey: ["success": true]]
             } else {
-                response.userInfo = [SFExtensionMessageKey: ["error": "failed to update settings"]]
+                response.userInfo = [SFExtensionMessageKey: ["error": "failed to update injection state"]]
             }
         }
         else if name == "TOGGLE_ITEM" {
             // the current status of the item to toggle comes in as 0 || 1
-            guard
+            if
                 let item = message?["item"] as? [String: Any],
                 let filename = item["filename"] as? String,
                 let current = item["disabled"] as? Int
-            else {
-                // TODO: handle error
-                return
-            }
-            let action = current == 0 ? "disable" : "enable"
-            if toggleFile(filename, action) {
-                response.userInfo = [SFExtensionMessageKey: ["success": true]]
+            {
+                let action = current == 0 ? "disable" : "enable"
+                if toggleFile(filename, action) {
+                    response.userInfo = [SFExtensionMessageKey: ["success": true]]
+                } else {
+                    response.userInfo = [SFExtensionMessageKey: ["error": "failed to toggle file"]]
+                }
             } else {
-                response.userInfo = [SFExtensionMessageKey: ["error": "failed to toggle file"]]
+                inBoundError = true
             }
         }
         else if name == "OPEN_SAVE_LOCATION" {
@@ -108,47 +109,45 @@ class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
             if let settings = getInitData() {
                 response.userInfo = [SFExtensionMessageKey: settings]
             } else {
-                // TODO: handle error better?
-                response.userInfo = [SFExtensionMessageKey: ["error": "failed"]]
+                response.userInfo = [SFExtensionMessageKey: ["error": "failed to get init data"]]
             }
         }
         else if name == "PAGE_ALL_FILES" {
             if let files = getAllFiles() {
                 response.userInfo = [SFExtensionMessageKey: files]
             } else {
-                // TODO: handle error better?
-                response.userInfo = [SFExtensionMessageKey: ["error": "failed"]]
+                response.userInfo = [SFExtensionMessageKey: ["error": "failed to get all files"]]
             }
         }
         else if name == "PAGE_SAVE" {
-            guard
+            if
                 let item = message?["item"] as? [String: Any],
                 let content = message?["content"] as? String
-            else {
-                // TODO: handle error
-                return
+            {
+                let saveResponse = saveFile(item, content)
+                response.userInfo = [SFExtensionMessageKey: saveResponse]
+            } else {
+                inBoundError = true
             }
-            let saveResponse = saveFile(item, content)
-            response.userInfo = [SFExtensionMessageKey: saveResponse]
         }
         else if name == "PAGE_TRASH" {
-            guard let item = message?["item"] as? [String: Any] else {
-                // TODO: handle error
-                return
-            }
-            if trashFile(item) {
-                response.userInfo = [SFExtensionMessageKey: ["success": true]]
+            if let item = message?["item"] as? [String: Any] {
+                if trashFile(item) {
+                    response.userInfo = [SFExtensionMessageKey: ["success": true]]
+                } else {
+                    response.userInfo = [SFExtensionMessageKey: ["error": "failed to trash file"]]
+                }
             } else {
-                response.userInfo = [SFExtensionMessageKey: ["error": "failed to trash file"]]
+                inBoundError = true
             }
         }
         else if name == "PAGE_UPDATE" {
-            guard let content = message?["content"] as? String else {
-                // TODO: handle error
-                return
+            if let content = message?["content"] as? String {
+                let updateResponse = getFileRemoteUpdate(content)
+                response.userInfo = [SFExtensionMessageKey: updateResponse]
+            } else {
+                inBoundError = true
             }
-            let updateResponse = getFileRemoteUpdate(content)
-            response.userInfo = [SFExtensionMessageKey: updateResponse]
         }
         else if name == "CANCEL_REQUESTS" {
             URLSession.shared.getAllTasks { tasks in
@@ -158,43 +157,46 @@ class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
             }
         }
         else if name == "PAGE_NEW_REMOTE" {
-            guard let url = message?["url"] as? String else {
-                // TODO: handle error
-                return
-            }
-            var r = [String: String]()
-            if !validateUrl(url) {
-                r = ["error": "Failed to get remote content, invalid url"]
-            } else if let content = getRemoteFileContents(url) {
-                r = ["content": content]
+            if let url = message?["url"] as? String {
+                if !validateUrl(url) {
+                    response.userInfo = [SFExtensionMessageKey: ["error": "Failed to get remote content, invalid url"]]
+                } else if let content = getRemoteFileContents(url) {
+                    response.userInfo = [SFExtensionMessageKey: content]
+                } else {
+                    response.userInfo = [SFExtensionMessageKey: ["error": "Failed to get remote content"]]
+                }
             } else {
-                r = ["error": "Failed to get remote content"]
+                inBoundError = true
             }
-            response.userInfo = [SFExtensionMessageKey: r]
         }
         else if name == "PAGE_UPDATE_SETTINGS" {
-            guard let settings = message?["settings"] as? [String: String] else {
-                // TODO: handle error
-                return
-            }
-            if updateSettings(settings) {
-                response.userInfo = [SFExtensionMessageKey: ["success": true]]
+            if let settings = message?["settings"] as? [String: String] {
+                if updateSettings(settings) {
+                    response.userInfo = [SFExtensionMessageKey: ["success": true]]
+                } else {
+                    response.userInfo = [SFExtensionMessageKey: ["error": "Failed to save settings to disk"]]
+                }
             } else {
-                response.userInfo = [SFExtensionMessageKey: ["error": "Failed to save settings to disk"]]
+                inBoundError = true
             }
         }
         else if name == "PAGE_UPDATE_BLACKLIST" {
-            guard let blacklist = message?["blacklist"] as? [String] else {
-                // TODO: handle error
-                return
-            }
-            var manifest = getManifest()
-            manifest.blacklist = blacklist
-            if updateManifest(with: manifest) {
-                response.userInfo = [SFExtensionMessageKey: ["success": true]]
+            if let blacklist = message?["blacklist"] as? [String] {
+                var manifest = getManifest()
+                manifest.blacklist = blacklist
+                if !updateManifest(with: manifest) {
+                    response.userInfo = [SFExtensionMessageKey: ["error": "Failed to save blacklist to disk"]]
+                } else {
+                    response.userInfo = [SFExtensionMessageKey: ["success": true]]
+                }
             } else {
-                response.userInfo = [SFExtensionMessageKey: ["error": "Failed to save blacklist to disk"]]
+                inBoundError = true
             }
+            
+        }
+        // send inBoundError if found
+        if inBoundError {
+            response.userInfo = [SFExtensionMessageKey: ["error": "Failed to parse inbound message"]]
         }
         context.completeRequest(returningItems: [response], completionHandler: nil)
     }
