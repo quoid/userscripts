2e39790625e20b9d9076b0681693265f58e57f64
quoid <justin@emlxl.com>
2022-01-30 14:11:18 -0500

    remove extraneous eslint disable

--- extension/Userscripts Extension/Resources/content.js
+++ extension/Userscripts Extension/Resources/content.js
@@ -354,13 +354,11 @@ function addStyle(css) {
     const pid = Math.random().toString(36).substring(1, 9);
     return new Promise(resolve => {
         const callback = e => {
-            // eslint-disable-next-line no-undef -- filename var accessible to the function at runtime
             if (e.data.pid !== pid || e.data.id !== uid || e.data.name !== "RESP_ADD_STYLE") return;
             resolve(e.data.response);
             window.removeEventListener("message", callback);
         };
         window.addEventListener("message", callback);
-        // eslint-disable-next-line no-undef -- filename var accessible to the function at runtime
         window.postMessage({id: uid, pid: pid, name: "API_ADD_STYLE", css: css});
     });
 }
