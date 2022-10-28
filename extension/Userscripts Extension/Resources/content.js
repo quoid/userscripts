c360244c7c374cbd645007755bf882c1e834c90a
quoid <justin@emlxl.com>
2022-10-28 13:34:49 -0400

    fix: properly wrap context-menu userscripts

--- extension/Userscripts Extension/Resources/content.js
+++ extension/Userscripts Extension/Resources/content.js
@@ -339,9 +339,11 @@ browser.runtime.sendMessage({name: "REQ_USERSCRIPTS"}, response => {
     }
     // save response locally in case CSP events occur
     data = response;
+    // combine regular and context-menu script for
+    const scripts = [...data.files.js, ...data.files.menu];
     // loop through each userscript and prepare for processing
-    for (let i = 0; i < data.files.js.length; i++) {
-        const userscript = data.files.js[i];
+    for (let i = 0; i < scripts.length; i++) {
+        const userscript = scripts[i];
         userscript.preCode = "";
         // pass references to the api methods as needed
         const gmMethods = [];
@@ -459,7 +461,14 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
             const item = data.files.menu[i];
             if (item.scriptObject.filename === filename) {
                 console.info(`Injecting ${filename} %c(js)`, "color: #fff600");
-                sendResponse({code: item.code});
+                sendResponse({
+                    code: wrapCode(
+                        item.preCode,
+                        item.requiredCode,
+                        item.code,
+                        filename
+                    )
+                });
                 return;
             }
         }
