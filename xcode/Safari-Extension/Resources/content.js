4854516b3dd8c53c852b45bb88468de34cc1a63e
ACTCD <101378590+ACTCD@users.noreply.github.com>
2023-04-30 10:18:04 +0800

    fix: onloadend may not a func

--- xcode/Safari-Extension/Resources/content.js
+++ xcode/Safari-Extension/Resources/content.js
@@ -178,11 +178,11 @@ const apis = {
                     }
                     // call userscript method
                     details[msg.name](msg.response);
-                    // all messages received
-                    // tell background it's safe to close port
-                    if (msg.name === "onloadend") {
-                        port.postMessage({name: "DISCONNECT"});
-                    }
+                }
+                // all messages received
+                // tell background it's safe to close port
+                if (msg.name === "onloadend") {
+                    port.postMessage({name: "DISCONNECT"});
                 }
             });
 
