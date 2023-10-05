97d2fde2de7eb99e1c2d469ee63bd9123981a873
ACTCD <101378590+ACTCD@users.noreply.github.com>
2023-10-05 19:02:19 +0800

    refactor: use vite mode instead of `NODE_ENV`
    https://github.com/vitejs/vite/issues/14372

--- src/page/main.js
+++ src/page/main.js
@@ -4,7 +4,7 @@ import "../shared/variables.css";
 import App from "./App.svelte";
 import "./cm.css";
 
-if (import.meta.env.DEV) { // vite feat that only import in dev mode
+if (import.meta.env.MODE === "development") { // vite feat that only import in dev mode
     const modules = import.meta.glob("../shared/dev.js", {eager: true});
     window.browser = modules["../shared/dev.js"].browser;
     console.info("DEV-ENV", import.meta.env, modules, browser);
--- src/popup/main.js
+++ src/popup/main.js
@@ -3,7 +3,7 @@ import "../shared/reset.css";
 import "../shared/variables.css";
 import App from "./App.svelte";
 
-if (import.meta.env.DEV) { // vite feat that only import in dev mode
+if (import.meta.env.MODE === "development") { // vite feat that only import in dev mode
     const modules = import.meta.glob("../shared/dev.js", {eager: true});
     window.browser = modules["../shared/dev.js"].browser;
     console.info("DEV-ENV", import.meta.env, modules, browser);
