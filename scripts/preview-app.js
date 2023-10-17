61f82b9119548cc1321634fa5b4d6edf0342527a
ACTCD <101378590+ACTCD@users.noreply.github.com>
2023-10-18 07:20:50 +0800

    refactor: update dev and build of ios app webview

---
+++ scripts/preview-app.js
@@ -0,0 +1,34 @@
+/**
+ * @file Preview App WebView resources using the Vite JavaScript API
+ * @see {@link https://vitejs.dev/guide/api-javascript.html|JavaScript API}
+ */
+
+import {preview} from "vite";
+
+/**
+ * Define default vite config options
+ * Disable auto resolving {@link vite.config.js}
+ * @see {@link https://vitejs.dev/config/|Config}
+ * @see {@link https://vitejs.dev/guide/api-javascript.html#inlineconfig|configFile}
+ */
+const defineConfig = {
+    base: "./",
+    configFile: false
+};
+
+/**
+ * Preview App-Shared WebView resources from xcode dist
+ */
+(async () => {
+    const previewServer = await preview({
+        ...defineConfig,
+        preview: {
+            // port: 4173,
+            open: "entry-app-webview.html"
+        },
+        build: {
+            outDir: "xcode/App-Shared/Resources/dist/"
+        }
+    });
+    previewServer.printUrls();
+})();
