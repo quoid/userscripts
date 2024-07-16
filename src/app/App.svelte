fee7589e02a0982a70ed9c9b8fe9fd45853c9cfb
ACTCD <101378590+ACTCD@users.noreply.github.com>
2024-07-17 03:38:07 +0800

    refactor: completely re-layout the entire page

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -2,6 +2,8 @@
 	import icon from "./img/icon.png";
 	import logo from "./img/logo.svg?raw";
 
+	const baseUrl = "https://github.com/quoid/userscripts";
+
 	let version = "v0.0.0";
 	let build = "(0)";
 	let directory = "init";
@@ -27,73 +29,89 @@
 </script>
 
 <main>
-	<img class="icon" src={icon} alt="Userscripts App Icon" draggable="false" />
-	<div class="logo">
+	<div class="section icons">
+		<img class="icon" src={icon} alt="Userscripts App Icon" draggable="false" />
 		<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-		{@html logo}
+		<div class="logo">{@html logo}</div>
+		<div class="version">
+			{#if import.meta.env.GIT_TAG && import.meta.env.GIT_COMMIT}
+				<a href="${baseUrl}/releases/tag/{import.meta.env.GIT_TAG}">
+					{import.meta.env.GIT_TAG}
+				</a>
+				(<a href="${baseUrl}/commit/{import.meta.env.GIT_COMMIT}">
+					{import.meta.env.GIT_COMMIT.slice(0, 7)}
+				</a>)
+			{:else}
+				<span>{version}</span>
+				<span>{build}</span>
+			{/if}
+		</div>
 	</div>
-	<div class="version">
-		{#if import.meta.env.GIT_TAG && import.meta.env.GIT_COMMIT}
+	<div class="section guide">
+		<p>
+			You can turn on the Userscripts iOS Safari extension in Settings or
+			Safari, then use the extension in Safari. Please refer to the "Usage"
+			section in the
 			<a
-				href="https://github.com/quoid/userscripts/releases/tag/{import.meta.env
-					.GIT_TAG}"
-			>
-				{import.meta.env.GIT_TAG}
-			</a>
-			(<a
-				href="https://github.com/quoid/userscripts/commit/{import.meta.env
-					.GIT_COMMIT}"
-			>
-				{import.meta.env.GIT_COMMIT.slice(0, 7)}
-			</a>)
-		{:else}
-			<span>{version}</span>
-			<span>{build}</span>
-		{/if}
+				href="${baseUrl}/blob/{import.meta.env.GIT_TAG ??
+					'main'}/README.md#usage">README of this version</a
+			>.
+		</p>
 	</div>
-	<br />
-	<p>
-		You can turn on the Userscripts iOS Safari extension in Settings or Safari,
-		then use the extension in Safari. Please refer to the "Usage" section in the
-		<a
-			href="https://github.com/quoid/userscripts/blob/{import.meta.env
-				.GIT_TAG ?? 'main'}/README.md#usage">README of this version</a
-		>.
-	</p>
-	<button id="change-directory" on:click={changeDirectory}>
-		Change Userscripts Directory
-	</button>
-	<div class="current">CURRENT DIRECTORY:</div>
-	<div id="directory">
-		<button class="link" on:click={openDirectory}>{directory}</button>
+	<div class="section action">
+		<button id="changedir" on:click={changeDirectory}>
+			Change Userscripts Directory
+		</button>
+		<div class="current">CURRENT DIRECTORY:</div>
+		<button id="directory" class="link" on:click={openDirectory}>
+			{directory}
+		</button>
 	</div>
-	<div class="footer">
-		<a
-			href="https://github.com/quoid/userscripts/blob/{import.meta.env
-				.GIT_TAG ?? 'main'}/README.md">Documentation</a
-		>
-		|
-		<a href="https://github.com/quoid/userscripts/discussions">Discussions</a>
-		|
-		<a href="https://github.com/quoid/userscripts/issues">Report an Issue</a>
-		|
-		<a href="https://github.com/quoid/userscripts#privacy-policy"
-			>Privacy Policy</a
-		>
+	<div class="section footer">
+		<div class="links">
+			<a href="${baseUrl}/blob/{import.meta.env.GIT_TAG ?? 'main'}/README.md"
+				>Documentation</a
+			>
+			<a href="${baseUrl}/discussions">Discussions</a>
+			<a href="${baseUrl}/issues">Report an Issue</a>
+			<a href="${baseUrl}#privacy-policy">Privacy Policy</a>
+		</div>
 	</div>
 </main>
 
 <style>
 	main {
-		align-items: center;
 		display: flex;
 		flex-direction: column;
-		height: 100%;
-		justify-content: center;
+		place-items: center;
+		place-content: center;
+		gap: min(2rem, 32px);
+		min-height: 100svh;
+		min-height: -webkit-fill-available;
 		padding: 16px;
 		text-align: center;
 		-webkit-touch-callout: none;
 		user-select: none;
+	}
+
+	.section {
+		display: flex;
+		place-items: center;
+		flex-direction: column;
+		gap: min(0.5rem, 16px);
+	}
+
+	.section.icons,
+	.section.footer {
+		flex: 1;
+		place-content: flex-end;
+	}
+
+	.section.footer .links {
+		display: flex;
+		flex-flow: row wrap;
+		place-content: center;
+		gap: min(0.5rem, 16px);
 	}
 
 	a {
@@ -101,22 +119,16 @@
 	}
 
 	.icon {
-		margin-top: 80px;
 		height: min(8rem, 256px);
 		width: min(8rem, 256px);
 	}
 
-	.logo {
-		align-items: flex-end;
-		display: flex;
-		margin: min(1rem, 32px) 0;
-	}
-
 	.logo :global(svg) {
+		display: flex;
 		height: min(1.5rem, 32px);
 	}
 
-	.footer,
+	.links,
 	.version,
 	.current {
 		color: var(--text-color-disabled);
@@ -125,7 +137,7 @@
 		letter-spacing: var(--letter-spacing-small);
 	}
 
-	button#change-directory {
+	button#changedir {
 		background-color: var(--color-blue);
 		border: none;
 		border-radius: var(--border-radius);
@@ -133,28 +145,19 @@
 		font: var(--text-default);
 		font-weight: 500;
 		letter-spacing: var(--letter-spacing-default);
-		margin: 2rem 0;
 		padding: 0.5rem 1rem;
 	}
 
-	button#change-directory:active {
+	button#changedir:active {
 		background-color: #6296c7;
 	}
 
-	#directory {
+	button#directory {
 		color: var(--editor-default);
 		font-family: var(--editor-font);
 		font-size: 0.875rem;
 		font-weight: 400;
 		word-break: break-all;
-	}
-
-	#directory button {
-		color: inherit;
 		text-decoration: none;
-	}
-
-	.footer {
-		margin-top: 80px;
 	}
 </style>
