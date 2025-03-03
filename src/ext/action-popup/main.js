580961576f0b2b73badeae1f6d21b050d6ecd2db
ACTCD <101378590+ACTCD@users.noreply.github.com>
2025-03-03 18:08:54 +0000

    chore: popup dev page adapt light theme

--- src/ext/action-popup/main.js
+++ src/ext/action-popup/main.js
@@ -20,19 +20,10 @@ body {
 	top: 20px;
 	left: 20px;
 	box-sizing: content-box;
-	border: 2px solid #0c0e0f;
+	border: 1px solid light-dark(#bebebe, #585c5f);
 	border-radius: 10px;
 	box-shadow: 2px 2px 20px rgba(0, 0, 0, 0.2);
-}
-body:before {
-	content: "";
-	position: absolute;
-	top: -1px;
-	left: -1px;
-	right: -1px;
-	bottom: -1px;
-	border: 1px solid #54575a;
-	border-radius: 9px;
+	overflow: hidden;
 }
 `;
 			browser.platform === "macos" && document.head.append(style);
