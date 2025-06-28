3469a4f399084c538672f35357f11f8c928b9853
ACTCD <101378590+ACTCD@users.noreply.github.com>
2025-06-28 23:25:47 +0000

    refactor: reduce repetition and simplify syntax

--- src/shared/colors.js
+++ src/shared/colors.js
@@ -5,16 +5,14 @@
 export function getColor(color) {
 	if (!color) return "color: inherit";
 	const isDark = matchMedia("(prefers-color-scheme: dark)").matches;
-	/** @type {string} */
-	let hex;
-	if (color === "blue") {
-		hex = isDark ? "#006fff" : "#317eff";
-	} else if (color === "green") {
-		hex = isDark ? "#60f36c" : "#2bb239";
-	} else if (color === "yellow") {
-		hex = isDark ? "#fff600" : "#b8722c";
-	} else {
-		return "";
+	const colors = {
+		blue: { dark: "#006fff", light: "#317eff" },
+		green: { dark: "#60f36c", light: "#2bb239" },
+		yellow: { dark: "#fff600", light: "#b8722c" },
+	};
+	if (color in colors) {
+		const hex = isDark ? colors[color].dark : colors[color].light;
+		return `color: ${hex}`;
 	}
-	return `color: ${hex}`;
+	return "";
 }
