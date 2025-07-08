599cc7bdc5c5cdda1a04a8b830248aa80135631b
ACTCD <101378590+ACTCD@users.noreply.github.com>
2025-07-08 21:59:41 +0000

    chore: add a trailing semicolon

--- src/shared/colors.js
+++ src/shared/colors.js
@@ -3,7 +3,7 @@
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/console#styling_console_output}
  */
 export const colors = {
-	inherit: "color: inherit",
+	inherit: "color: inherit;",
 	blue: "color: light-dark( #317eff, #006fff);",
 	green: "color: light-dark( #2bb239, #60f36c);",
 	yellow: "color: light-dark( #b8722c, #fff600);",
